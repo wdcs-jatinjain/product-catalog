965e26d8f84aa79a60be93385540cdfc3ce2bb04
Jatin Jain Saraf <138874573+wdcs-jatinjain@users.noreply.github.com>
2024-01-12 18:02:18 +0530

    Delete admin/src/app/Components/AdminInterface/index.d.ts

--- admin/src/app/Components/AdminInterface/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface InputProps {
-    type: string;
-    value: string;
-    name: string;
-    disabled?: boolean;
-    placeholder?: string;
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  }
-  
