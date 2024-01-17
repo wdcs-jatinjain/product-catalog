cb385527738da611c95e032341639b20a92beb61
wdcs-harshitshah <142909834+wdcs-harshitshah@users.noreply.github.com>
2024-01-17 17:33:18 +0530

    Feature/pc 2 | added api for admin login (#25)
    * Added customer model Schema
    
    * admin login api
    
    * Delete server/package-lock.json
    
    * update folder structure
    
    ---------
    
    Co-authored-by: harshit shah <harshit.shah@codezeros>
    Co-authored-by: Jatin Jain Saraf <138874573+wdcs-jatinjain@users.noreply.github.com>
    Co-authored-by: supra-jatinjainsaraf <j.saraf@supraoracles.com>

---
+++ server/src/controllers/user.routes.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import checkAdminLogin from "../views/user/checkadminlogin";
+import registerUser from '../views/user/index';
+
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/api/admin/login", checkAdminLogin);
+
+export default router;
