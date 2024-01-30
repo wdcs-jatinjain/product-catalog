f04bdac4b4059ab51fd74bbcd34f4fea15cb8e25
wdcs-harshitshah <harshit.shah@codezeros.com>
2024-01-30 09:55:14 +0530

    updated

--- server/src/controllers/customer/CustomerValidations.ts
+++ server/src/controllers/customer/CustomerValidations.ts
@@ -5,6 +5,6 @@ export const validateCustomerRegistration = Joi.object().keys({
     email: Joi.string().email().required().messages({"any.requires":"Please provide valid EmailID"}),
     password: Joi.string().required().messages({"any.requires":"Please provide Correct Password format"}),
     phone: Joi.string().required().messages({"any.requires":"Please provide valid phone number format"}),
-    zipcode: Joi.string().required().messages({"any.requires":"Please provide valid Zipcode"}), 
+    zipCode: Joi.string().required().messages({"any.requires":"Please provide valid Zipcode"}), 
 
 })
