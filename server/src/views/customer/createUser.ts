6af3595cc09f1e594297cdf59a8060df6d303a1e
wdcs-harshitshah <142909834+wdcs-harshitshah@users.noreply.github.com>
2024-01-31 10:03:05 +0530

    Feature/pc 5 server (#30)
    * fixIssues | fixed route handlers issues
    
    * admin Login
    
    * new customer registration api
    
    * Register API Error handling
    
    * File Structure
    
    * Files
    
    * Delete server/src/controllers/customer/checkCustomerValidations.ts
    
    * Delete server/src/controllers/customer/checkcustomerregister.ts
    
    * Delete server/src/controllers/web/customer/checkcustomerregister.ts
    
    * Delete server/src/controllers/web/index.ts
    
    * Files Deleted
    
    * Files Deleted
    
    * File names Changed
    
    * Files Deleted
    
    * feature/PC-5 | fixed issue in harshit code
    
    * Changes Done
    
    * conflict resolved
    
    * changes done
    
    * updated
    
    * Removed folder_name from repository
    
    * issue resolved
    
    * Fixed Issue
    
    ---------
    
    Co-authored-by: supra-jatinjainsaraf <j.saraf@supraoracles.com>

--- server/src/views/customer/createUser.ts
+++ server/src/views/customer/createUser.ts
@@ -4,36 +4,36 @@ import bcrypt from "bcryptjs";
 export default async function createUser(req: any, res: any){
     try {
         const { name, email, password, phone, zipCode } = req.body;
-        const existingCustomer =  await CustomerModel.findOne({ email });
+
+        const existingCustomer = await CustomerModel.findOne({ email });
         if (existingCustomer) {
-          return res.status(400).json({
-            status: "Failure",
-            message: "User already exists. Please login.",
-          });
+            return {
+                status: "Failure",
+                message: "User already exists. Please login.",
+            };
         }
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
         const newCustomer = await CustomerModel.create({
-          name,
-          email,
-          password: hashedPassword,
-          phone,
-          zipCode,
+            name,
+            email,
+            password: hashedPassword,
+            phone,
+            zipCode,
         });
 
-        return res.status(200).json({
-          status: "Success",
-          message: "User registered.",
-          data: {
-            id:newCustomer._id
-          }
-        });
-      } catch (error) {
+
+        return {
+            status: "Success",
+            message: "User registered.",
+            data: {
+                id: newCustomer._id
+            }
+        };
+    } catch (error:any) {
         console.error("An error occurred while registering the user:", error);
-        return res.status(500).json({
-          status: "Failure",
-          message: "An error occurred while registering the user.",
-        });
-      }
+
+        throw new Error(error.message || "Unknown error")
+    }
 }
