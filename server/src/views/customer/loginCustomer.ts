0e25ee43c214837b95bc9141169ee9f081bbf569
wdcs-harshitshah <harshit.shah@codezeros.com>
2024-02-06 12:37:08 +0530

    server customer login

--- server/src/views/customer/loginCustomer.ts
+++ server/src/views/customer/loginCustomer.ts
@@ -10,18 +10,18 @@ const loginCustomer = async (req:any, res:any, _next:any) => {
         console.log('second', email, password)
         const existingUser = await Customer.findOne({ email: email });
         if (!existingUser) {
-            return res.status(400).json( {status:"failure",message:"User not found. Please sign up."} );
+            return  {status:"failure",message:"User not found. Please sign up."} ;
         }
         const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
         console.log('first', isPasswordCorrect)
         if (!isPasswordCorrect) {
-            return res.status(400).json( {status:"failure", message:"Invalid email or password."} );
+            return {status:"failure", message:"Invalid email or password."} ;
         }
         
-        return res.status(200).json({status:"success",message:"Successfully logged in"});
+        return {status:"success",message:"Successfully logged in"};
     } catch (error:any) {
-        console.error("Error logging in:", error);
-        return res.status(500).json({status:"failure", message:"Internal server error"} );
+        console.error("An error occurred while Logging of the Customer:", error);
+        throw new error;
     }
 };
 
