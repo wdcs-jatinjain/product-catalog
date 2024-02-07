6c2e67aa9b862446646602a4932b6591bea43f4d
wdcs-harshitshah <harshit.shah@codezeros.com>
2024-02-07 15:34:41 +0530

    login token mngt

--- server/src/views/customer/generateToken.ts
+++ server/src/views/customer/generateToken.ts
@@ -1,26 +1,26 @@
-import jwt from "jsonwebtoken";
-import Customer from "../../models/customer";
-import { SECRET_KEY } from "../../config";
+// import jwt from "jsonwebtoken";
+// import Customer from "../../models/customer";
+// import { SECRET_KEY } from "../../config";
 
-const checkCustomerAuth = async (req: any, res: any, next: any) => {
-  let token;
-  const { authorization } = req.headers;
-  if (authorization && authorization.startsWith("Bearer")) {
-    try {
-      token = authorization.split(" ")[1];
-      const tokenPayload: any = jwt.verify(token, SECRET_KEY as string);
-      const { customerID } = tokenPayload;
-      req.customer = await Customer.findById(customerID).select("-password");
-      next();
-    } catch (error) {
-      console.log(error);
-      res.status(401).send({ status: "failed", message: "Unauthorized User" });
-    }
-  }
-  if (!token) {
-    res
-      .status(401)
-      .send({ status: "failed", message: "Unauthorized User, No Token" });
-  }
-};
-export default checkCustomerAuth;
+// const checkCustomerAuth = async (req: any, res: any, next: any) => {
+//   let token;
+//   const { authorization } = req.headers;
+//   if (authorization && authorization.startsWith("Bearer")) {
+//     try {
+//       token = authorization.split(" ")[1];
+//       const tokenPayload: any = jwt.verify(token, SECRET_KEY as string);
+//       const { customerID } = tokenPayload;
+//       req.customer = await Customer.findById(customerID).select("-password");
+//       next();
+//     } catch (error) {
+//       console.log(error);
+//       res.status(401).send({ status: "failed", message: "Unauthorized User" });
+//     }
+//   }
+//   if (!token) {
+//     res
+//       .status(401)
+//       .send({ status: "failed", message: "Unauthorized User, No Token" });
+//   }
+// };
+// export default checkCustomerAuth;
--- server/src/views/customer/loginCustomer.ts
+++ server/src/views/customer/loginCustomer.ts
@@ -6,60 +6,96 @@ import { SECRET_KEY } from "../../config";
 const loginCustomer = async (req: any, res: any, _next: any) => {
   const { email, password } = req.body;
   const { token } = req.headers;
-
+console.log(req.headers)
   try {
+    let existingCustomer;
+console.log(token)
     if (token) {
-      const decodedToken: any = jwt.verify(token, SECRET_KEY as string);
-      const { customerID } = decodedToken;
+      
+      if (token.startsWith("Bearer")) {
+        existingCustomer = await Customer.findOne({ email: email });
+console.log(existingCustomer)
+        if (existingCustomer) {
+          const checkToken = token.split(" ")[1];
+          const decodedToken: any = jwt.decode(checkToken);
 
-      const existingUser = await Customer.findById(customerID);
-      if (!existingUser) {
+          if (decodedToken && decodedToken.exp && Date.now() / 1000 < decodedToken.exp) {
+
+          const { customerID } :any= jwt.verify(
+            checkToken,
+            SECRET_KEY as string
+          );
+console.log(customerID)
+
+console.log(customerID === existingCustomer._id.toString())
+          if (customerID === existingCustomer._id.toString()) {
+            return {
+              status: "success",
+              message: "User found in the database.",
+              token: token,
+            };
+          }
+
+        } else {
+          return {
+            status: "failure",
+            message: "Token has expired.",
+          };
+        }
+        } else {
+          return {
+            status: "failure",
+            message: "User not found in the database.",
+          };
+        }
+      } else {
         return {
           status: "failure",
-          message: "User not found. Please sign up.",
+          message: "Invalid token format.",
         };
       }
-      const isPasswordCorrect = bcrypt.compareSync(
-        password,
-        existingUser.password
-      );
-      if (!isPasswordCorrect) {
-        return { status: "failure", message: "Invalid email or password." };
-      }
-
-      return { status: "success", message: "Successfully logged in" };
     } else {
-      const existingUser = await Customer.findOne({ email: email });
-      if (!existingUser) {
+      existingCustomer = await Customer.findOne({ email: email });
+      if (!existingCustomer) {
         return {
           status: "failure",
-          message: "User not found. Please sign up.",
+          message: "User not found in the database.",
         };
       }
-
       const isPasswordCorrect = bcrypt.compareSync(
         password,
-        existingUser.password
+        existingCustomer.password
       );
       if (!isPasswordCorrect) {
-        return { status: "failure", message: "Invalid email or password." };
+        return {
+          status: "failure",
+          message: "Invalid email or password.",
+        };
       }
-      const expiresIn = 7200;
-      const newToken = jwt.sign(
-        { customerID: existingUser._id },
-        SECRET_KEY as string,
-        { expiresIn }
-      );
-      return {
-        status: "success",
-        message: "Successfully logged in",
-        token: newToken,
-      };
+      if (!existingCustomer.token) {
+        const newToken = jwt.sign(
+          { customerID: existingCustomer._id },
+          SECRET_KEY as string,
+          { expiresIn: "2d" }
+        );
+        return {
+          status: "success",
+          message: "Customer found in the database but New Token Generated.",
+          token: newToken,
+        };
+      } else {
+        return {
+          status: "success",
+          message: "User found in the database.",
+          token: existingCustomer.token,
+        };
+      }
     }
   } catch (error: any) {
-    console.error("An error occurred while Logging of the Customer:", error);
-    throw new Error("An error occurred while Logging in the Customer");
+    console.error("An error occurred while logging in the customer:", error);
+    throw new Error("An error occurred while logging in the customer");
   }
 };
 
 export default loginCustomer;
+
